109ae00ce868442e7cee76d7e76e8bfff26ad017
Ltrain <landonkline612@gmail.com>
2025-11-26 15:41:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+function randomPrompt() {
+  const i = Math.floor(Math.random() * prompts.length);
+  return prompts[i];
+}
+
+document.getElementById("btn").addEventListener("click", () => {
+  document.getElementById("prompt").textContent = randomPrompt();
+});
 
